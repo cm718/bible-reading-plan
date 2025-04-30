bbc02a71c6feea8acfc7f53ea69e84acdf0a1103
Chris <cm718@icloud.com>
2025-04-30 14:55:36 -0700

    Trying to fix header plus logs added

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import ActiveTab from './components/ActiveTab';
 import './App.css';
 
 const App: React.FC = () => {
+  console.log('Standalone?', window.matchMedia('(display-mode: standalone)').matches);
   const [activeTab, setActiveTab] = useState<string>('home'); // State to track the active tab
   return (
     <div className="app-container">
